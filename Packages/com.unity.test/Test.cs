37b3d0732a20dc1db8fbb5122ccb19123c352eb1
D-buger <o3oduck@naver.com>
2025-04-27 21:49:39 +0900

    유니티 내장 패키지 수정에 대한 고민

---
+++ Packages/com.unity.test/Test.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace TMPro
+{
+    public class Test
+    {
+        TextMeshPro m_TextMeshPro;
+
+        void Function()
+        {
+            m_TextMeshPro._SortingLayer = 1;
+        }
+    }
+}
